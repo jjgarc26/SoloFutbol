2bdb349db9bac7b682158be36385636cbc1eb6ba
Jose Garcia <mendoza.joser51@gmail.com>
2024-08-22 21:08:59 -0700

    save work

--- solo-client/src/pages/accounts/login/index.tsx
+++ solo-client/src/pages/accounts/login/index.tsx
@@ -14,6 +14,7 @@ function Login() {
   function onSignUpClick() {
     router.push("./signup");
   }
+
   function onLogin() {
     // API call will be made here
     console.log("Logging in....");
@@ -21,30 +22,30 @@ function Login() {
 
   return (
     <Card>
-      <div className={style.login}>
-        <div className={style.loginInput}>
-          <Input
-            labelName="Username"
-            placeHolder="Username"
-            type="text"
-            onChange={setUserName}
-          />
-        </div>
-        <div className={style.loginInput}>
-          <Input
-            labelName="Password"
-            placeHolder="Password"
-            type="password"
-            onChange={setPassword}
-          />
-        </div>
-        <div>
+      <div className={style.form}>
+        <div className={style.login}>
+          <div className={style.loginInput}>
+            <Input
+              labelName="Username"
+              placeHolder="Username"
+              type="text"
+              onChange={setUserName}
+            />
+          </div>
+          <div className={style.loginInput}>
+            <Input
+              labelName="Password"
+              placeHolder="Password"
+              type="password"
+              onChange={setPassword}
+            />
+          </div>
           <Button buttonName="Login" onClick={onLogin} />
         </div>
-      </div>
-      <div>
-        <p>Need an account?</p>
-        <button onClick={onSignUpClick}>Sign up here</button>
+        <div className={style.signUp}>
+          <h1 className={style.heading1}>--- Need an account? ---</h1>
+          <Button buttonName="Sign up" onClick={onSignUpClick} />
+        </div>
       </div>
     </Card>
   );
